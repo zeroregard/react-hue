26ce524f8808699404ba5d569a7ff501636f5c52
Mathias <mathias_sn@hotmail.com>
2021-10-05 21:07:13 +0200

    styling so hue slider looks like temp/brightness sliders

--- src/components/controls/HueController.tsx
+++ src/components/controls/HueController.tsx
@@ -1,4 +1,6 @@
+import { Stack } from "@mui/material";
 import { HuePicker } from "react-color";
+import { IoIosColorPalette } from "react-icons/io";
 import { lightStateToHue, toAPIHue } from "../../helpers/colorHelper";
 import { setHue } from "../../helpers/hueApi";
 
@@ -6,11 +8,14 @@ export function HueController(props: any) {
     return (
         <div>
             {props.light?.state.hue !== undefined &&
-                <div style={{ marginBottom: 16}}>
-                    <HuePicker
-                        color={lightStateToHue(props.light.state)}
-                        onChangeComplete={(color, _e) => setHue(props.id, toAPIHue(color.hsl.h))}
-                    />
+                <div style={{ marginBottom: 16 }}>
+                    <Stack spacing={2} direction="row" alignItems="center">
+                        <IoIosColorPalette />
+                        <HuePicker
+                            color={lightStateToHue(props.light.state)}
+                            onChangeComplete={(color, _e) => setHue(props.id, toAPIHue(color.hsl.h))}
+                        />
+                    </Stack>
                 </div>}
         </div>
     );
